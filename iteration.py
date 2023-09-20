e754dee086e01c339be21642be01b2a1794517d3
wpgoble <will.goble@gmail.com>
2023-09-20 12:17:52 -0400

    adds iteration demo to repo

---
+++ iteration.py
@@ -0,0 +1,67 @@
+### basic for loop
+##for i in range(10):
+##    print("hello class")
+##    print("current iteration = ", i)
+##
+### print 21 - 30
+##for i in range(21, 31):
+##    print("current value = ", i)
+
+# count down from 20 to 0
+##for i in range(20, -1, -1):
+##    print(i)
+
+# find the summation for values 1 to 10
+##answer = 0
+##for i in range(1, 11):
+####    answer += i
+##    answer = answer + i
+##print("Total is =", answer)
+
+# find the product of i^2 for 1 to 10
+##answer = 1
+##for i in range(1, 11):
+##    answer  *= i**2
+####    answer = answer * i**2 
+##print(answer)
+
+##for i in range(3, 12, 2):
+##    print("Here is an odd number", i)
+##    
+##for i in range(3, 12):
+##    if i % 2 == 1:
+##        print(i)
+##
+##
+##
+##name = "Josh"
+##for i in name:
+##    print(i)
+##
+##print(name[0])
+
+
+
+
+for i in range(3):
+    print("Outer loop is at iteration", i)
+    for j in range(2):
+        print("Inner loop is at iteration", j)
+        print("i + j =", i+j)
+    print("End of outer loop iteration, i =", i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+
